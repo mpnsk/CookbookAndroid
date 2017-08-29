87b13b01e4bd7dae4d453fbba694bb7d592e9609
mpnsk <manuel.paunoski@gmail.com>
2017-08-29 19:22:27 +0200

    Setup project for DataBinding

--- DataBindingTwoWay/app/src/main/java/com/paunoski/manuel/databinding_two_way/MainActivity.java
+++ DataBindingTwoWay/app/src/main/java/com/paunoski/manuel/databinding_two_way/MainActivity.java
@@ -1,13 +1,16 @@
 package com.paunoski.manuel.databinding_two_way;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.paunoski.manuel.databinding_two_way.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
