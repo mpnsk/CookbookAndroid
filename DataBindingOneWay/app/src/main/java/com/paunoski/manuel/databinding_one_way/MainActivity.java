032a89410fa44bd3af9b0847af6d0bf94e59a7f5
mpnsk <manuel.paunoski@gmail.com>
2017-08-29 13:51:14 +0200

    init empty project

---
+++ DataBindingOneWay/app/src/main/java/com/paunoski/manuel/databinding_one_way/MainActivity.java
@@ -0,0 +1,13 @@
+package com.paunoski.manuel.databinding_one_way;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
