7cf0a88c2d9f9d54c2b40a3fe96658db3e3e4fe5
mpnsk <manuel.paunoski@gmail.com>
2017-08-29 14:26:50 +0200

    Update gradle wrapper, shift around some whitespaces

--- DataBindingOneWay/app/src/main/java/com/paunoski/manuel/databinding_one_way/MainActivity.java
+++ DataBindingOneWay/app/src/main/java/com/paunoski/manuel/databinding_one_way/MainActivity.java
@@ -3,6 +3,7 @@ package com.paunoski.manuel.databinding_one_way;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 import com.paunoski.manuel.databinding_one_way.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
