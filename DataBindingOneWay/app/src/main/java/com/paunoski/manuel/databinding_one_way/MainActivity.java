4304b2004ce30a4fece71768ab15aa57b7e33cb2
mpnsk <manuel.paunoski@gmail.com>
2017-08-29 14:16:42 +0200

    Add One-Way DataBinding

--- DataBindingOneWay/app/src/main/java/com/paunoski/manuel/databinding_one_way/MainActivity.java
+++ DataBindingOneWay/app/src/main/java/com/paunoski/manuel/databinding_one_way/MainActivity.java
@@ -1,13 +1,16 @@
 package com.paunoski.manuel.databinding_one_way;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.paunoski.manuel.databinding_one_way.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.textHelloWorld.setText("One-Way DataBinding is running!");
     }
 }
